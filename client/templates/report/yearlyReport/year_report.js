c735db2cfbba11006994cd159695209b10e4cde7
meghna1711 <meghnagogna111@gmail.com>
2015-09-30 12:51:31 +0530

    show year report is completed

--- client/templates/report/yearlyReport/year_report.js
+++ client/templates/report/yearlyReport/year_report.js
@@ -17,10 +17,12 @@ Template.yearReport.events({
         e.preventDefault();
 
         var year = $('#selectYear').val(),
-            projectKey = $('selectProject').val();
+            projectKey = $('#selectProject').val();
 
         console.log(">>>>>>>>>>>>>>>year>>>>>>>>>>>>>>>",year);
         getCommitsReportForTheYear(projectKey , year);
+
+        Router.go('showReport' , {projectKey : projectKey});
     }
 });
 
@@ -28,7 +30,31 @@ var  getCommitsReportForTheYear = function(projectKey , year){
     var yearCommits = new yearlyReport();
     yearCommits.Year = year;
     var commitsResult, regex = new RegExp("^"+year+'(.*)');
-    console.log(regex);
+    console.log(regex +"  " + projectKey);
     commitsResult = Commits.find({projectId : projectKey , timestamp : {$regex : regex}}).fetch();
     console.log(commitsResult);
+
+    if(commitsResult){
+        yearCommits.Total=commitsResult.length;
+        commitsResult.forEach(function (value) {
+            var commitDate=new Date(value.timestamp);
+            var index=commitDate.getMonth();
+            yearCommits.Month[index].Total+=1;
+            if(value.added.length>0){
+                yearCommits.Added+=1;
+                yearCommits.Month[index].Added+=1;
+            }
+            if(value.removed.length>0){
+                yearCommits.Removed+=1;
+                yearCommits.Month[index].Removed+=1;
+            }
+            if(value.modified.length>0){
+                yearCommits.Modified+=1;
+                yearCommits.Month[index].Modified+=1;
+            }
+        });
+
+        reportService.setReportData(yearCommits);
+        console.log(yearCommits);
+    }
 };
